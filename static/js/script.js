f872aa5539cbd04477da8ddda100810666dceb40
QuelleQim <cathrine.karl@gmail.com>
2020-12-30 22:13:14 +0000

    Add categories dropdown to add_review.html and functionality in app.py

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
-    
+    $('select').formSelect();
 });
 
 
